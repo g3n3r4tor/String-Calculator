4032487ebb10b1a3ec0934f3203bbe980f041456
Ingibergur Sindri Stefnisson <ingibergur90@gmail.com>
2014-10-12 02:06:41 +0000

    Nr 1

---
+++ String-Calculator/CalculatorTests/UnitTest1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using String_Calculator;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestEmptyString()
+        {
+            // arrange
+            Calculator calc = new Calculator();
+            string s = "";
+
+            // act
+            int b = 0;
+            
+
+            // assert
+            int a = calc.Add(s);
+            Assert.AreEqual(b, a);
+        }
+        [TestMethod]
+        public void TestOneNumber()
+        {
+            // arrange
+            Calculator calc = new Calculator();
+            string s = "1";
+
+            // act
+            int b = 1;
+
+
+            // assert
+            int a = calc.Add(s);
+            Assert.AreEqual(b, a);
+        }
+        [TestMethod]
+        public void TestOneRandomNumber()
+        {
+            // arrange
+            Calculator calc = new Calculator();
+            string s = "";
+            Random rand = new Random();
+            int rnd = rand.Next(1000);
+            s = rnd.ToString();
+
+            // act
+            int b = rnd;
+
+
+            // assert
+            int a = calc.Add(s);
+            Assert.AreEqual(b, a);
+        }
+        [TestMethod]
+        public void TestTwoNumbers()
+        {
+            // arrange
+            Calculator calc = new Calculator();
+            string s = "1,2";
+
+            // act
+            int b = 3;
+
+
+            // assert
+            int a = calc.Add(s);
+            Assert.AreEqual(b, a);
+        }
+        [TestMethod]
+        public void TestTwoRandomNumbers()
+        {
+            // arrange
+            Calculator calc = new Calculator();
+            string s = "";
+            Random rand = new Random();
+            int rnd = rand.Next(1000);
+            int rnd2 = rand.Next(1000);
+            s = rnd.ToString() + "," + rnd2.ToString();
+            // act
+            int b = rnd + rnd2;
+
+
+            // assert
+            int a = calc.Add(s);
+            Assert.AreEqual(b, a);
+        }
+        [TestMethod]
+        public void TestNaN()
+        {
+            // arrange
+            Calculator calc = new Calculator();
+            string s = "This is NaN";
+
+            // act
+            bool thrown = false;
+
+            try
+            {
+                calc.Add(s);
+            }
+            catch (Exception e)
+            {
+                thrown = true;
+            }
+            // assert
+            Assert.IsTrue(thrown);
+
+        }
+
+    }
+}
---
+++ String-Calculator/String-Calculator/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Calculator
+{
+    public class Calculator
+    {
+        public int Add(string numbers)
+        {
+            if(numbers == "")
+                return 0;
+            string[] a = numbers.Split(',');
+            int[] b = new int[a.Length];
+            
+            for (int i = 0; i < a.Length; i++)
+            {
+                b[i] = Convert.ToInt32(a[i]);
+            }
+            if (b.Length == 1)
+                return b[0];
+            int sum = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                sum += b[i];
+            }
+            return sum;
+        }
+
+    }
+}
